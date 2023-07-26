e0c27313f3b7f56fe59a20bdf20e0d6c9a5a592a
samadmemon991 <samadmemon991@gmail.com>
2023-07-26 13:52:17 +0500

    user sign up completed

--- models/user.model.js
+++ models/user.model.js
@@ -5,6 +5,10 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    userEmail: {
+        type: String,
+        required: true
+    },
     password: {
         type: String,
         required: true
